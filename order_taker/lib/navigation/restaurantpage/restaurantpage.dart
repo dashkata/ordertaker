0e43e7c140088d5247b7d336689fc13b9bf64b5a
dashkata <alexandur04@abv.bg>
2022-03-26 15:55:41 +0200

    Added the restaurant page

---
+++ order_taker/lib/navigation/restaurantpage/restaurantpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RestaurantPage extends StatelessWidget {
+  const RestaurantPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Restaurant Page"),
+    );
+  }
+}
