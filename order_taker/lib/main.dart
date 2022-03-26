90cd2b649ef5f20e9806a8dd1f4f0e8ea4b69617
dashkata <alexandur04@abv.bg>
2022-03-26 15:55:41 +0200

    Added the restaurant page

--- order_taker/lib/main.dart
+++ order_taker/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:order_taker/navigation/registerpage/registerpage.dart';
+import 'package:order_taker/navigation/restaurantpage/restaurantpage.dart';
 import 'navigation/loginpage/loginpage.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/register',
+    initialRoute: '/restaurants',
     routes: {
       '/login': (context) => LoginPage(),
       '/register': (context) => RegisterPage(),
+      '/restaurants': (context) => RestaurantPage()
     },
   ));
 }
