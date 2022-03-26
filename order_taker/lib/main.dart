cff5ac59e7715c796c32f6e96f702910f46988bf
dashkata <alexandur04@abv.bg>
2022-03-26 15:32:43 +0200

    Added the login and register pages and the branches for them

--- order_taker/lib/main.dart
+++ order_taker/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:order_taker/navigation/registerpage/registerpage.dart';
+import 'navigation/loginpage/loginpage.dart';
 
 void main() {
   runApp(MaterialApp(
-    
-
-
+    initialRoute: '/register',
+    routes: {
+      '/login': (context) => LoginPage(),
+      '/register': (context) => RegisterPage(),
+    },
   ));
 }
