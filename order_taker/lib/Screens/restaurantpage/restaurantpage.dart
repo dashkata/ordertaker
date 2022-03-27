1507fe515a4fafcf6800c8bc7f82ddf7188bdb13
dashkata <alexandur04@abv.bg>
2022-03-27 23:26:01 +0300

    Added some cards to the restaurant page

--- order_taker/lib/Screens/restaurantpage/restaurantpage.dart
+++ order_taker/lib/Screens/restaurantpage/restaurantpage.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+
+import 'package:getwidget/getwidget.dart';
 import 'package:order_taker/Screens/customwidgets.dart';
 
-class RestaurantPage extends StatelessWidget {
+class RestaurantPage extends StatefulWidget {
   const RestaurantPage({Key? key}) : super(key: key);
 
+  @override
+  State<RestaurantPage> createState() => _RestaurantPageState();
+}
+
+class _RestaurantPageState extends State<RestaurantPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           CustomWidgets.returnBackground(),
+          SafeArea(
+            child: Column(
+              children: [
+                GFCard(
+                  boxFit: BoxFit.cover,
+                  titlePosition: GFPosition.start,
+                  image: Image.asset('your asset image'),
+                  title: const GFListTile(
+                    title: Text('Pizza Don Vito'),
+                  ),
+                  content: Text("A nice pizza restaurant"),
+                  buttonBar: GFButtonBar(
+                    children: <Widget>[
+                      GFButton(
+                        onPressed: () {},
+                        text: 'Chooose',
+                      ),
+                    ],
+                  ),
+                ),
+                GFCard(
+                  boxFit: BoxFit.cover,
+                  titlePosition: GFPosition.start,
+                  image: Image.asset('your asset image'),
+                  title: const GFListTile(
+                    title: Text('Pizza Don Vito'),
+                  ),
+                  content: Text("A nice pizza restaurant"),
+                  buttonBar: GFButtonBar(
+                    children: <Widget>[
+                      GFButton(
+                        onPressed: () {},
+                        text: 'Chooose',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
