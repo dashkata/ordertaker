290cf39ea8191c5b342cae8cda636b993fe014dc
dashkata <alexandur04@abv.bg>
2022-08-22 15:44:16 +0300

    merged menu screen into dev

--- order_taker/lib/controllers/menu_screen_state_controller.dart
+++ order_taker/lib/controllers/menu_screen_state_controller.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:order_taker/models/menu_item_model.dart';
 import 'package:order_taker/models/order_model.dart';
-<<<<<<< HEAD
 import 'package:order_taker/models/reservation_model.dart';
-=======
->>>>>>> c587147dffd54cd4c3bb6465f84747fd6ca5339f
 
 import '../providers/repository_providers.dart';
 
@@ -23,24 +20,14 @@ class MenuScreenNotifier extends StateNotifier<List<OrderItem>> {
     ];
   }
 
-<<<<<<< HEAD
   void completeOrder(Reservation reservation) {
     ref.read(firestoreRepositoryProvider).completeOrder(
-=======
-  void completeOrder() {
-    ref.read(firestoreRepositoryProvider).completeOrder(
-          'Pizza Don Vito',
-          '1',
->>>>>>> c587147dffd54cd4c3bb6465f84747fd6ca5339f
           Order(
             id: 1,
             menuItems: state,
           ),
-<<<<<<< HEAD
           ref.watch(authRepositoryProvider).getCurrentUser()!.uid,
           reservation,
-=======
->>>>>>> c587147dffd54cd4c3bb6465f84747fd6ca5339f
         );
     state = [];
   }
