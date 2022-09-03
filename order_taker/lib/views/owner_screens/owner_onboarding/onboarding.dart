9f4c89908fbf9f3cc167368d94b90aa1a1e5a14a
dashkata <alexandur04@abv.bg>
2022-09-04 01:15:35 +0300

    Added the 2nd page from the onboarding

--- order_taker/lib/views/owner_screens/owner_onboarding/onboarding.dart
+++ order_taker/lib/views/owner_screens/owner_onboarding/onboarding.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:order_taker/views/project_widgets.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
+
+  //address, open hours, payment methods, website, phone number
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +35,72 @@ class OnboardingScreen extends StatelessWidget {
               ),
             ),
           ),
+          bodyWidget: Form(
+            child: Column(
+              children: [
+                TextFields(
+                  hintText: 'Restaurant address',
+                  icon: Icons.location_city,
+                  obscure: false,
+                  inputType: TextInputType.streetAddress,
+                  func: (val) {},
+                ),
+                TextFields(
+                  hintText: 'Restaurant working hours',
+                  icon: Icons.watch_later,
+                  obscure: false,
+                  inputType: TextInputType.none,
+                  func: (val) {},
+                ),
+                TextFields(
+                  hintText: 'Restaurant website',
+                  icon: Icons.web,
+                  obscure: false,
+                  inputType: TextInputType.streetAddress,
+                  func: (val) {},
+                ),
+                TextFields(
+                  hintText: 'Restaurant phone number',
+                  icon: Icons.phone,
+                  obscure: false,
+                  inputType: TextInputType.phone,
+                  func: (val) {},
+                )
+              ],
+            ),
+          ),
+        ),
+        PageViewModel(
+          titleWidget: Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Center(
+              child: Text(
+                'Add the menu for your restaurant.',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+          ),
           bodyWidget: Column(
+            children: [
+              NormalButtons(
+                buttonText: 'Add a menu item',
+                buttonFunc: () => showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Center(
+                      child: Text(
+                        'Add a menu item',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                    ),
+                    content: Column(
 
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
